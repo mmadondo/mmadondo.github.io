359ebba35c57bcbc08ca7831c0e1e1faae5e877d
Malvern <mmadondo@users.noreply.github.com>
2019-11-18 20:11:51 -0500

    Updating header and adding background mode toggling..still WIP

---
+++ script.js
@@ -0,0 +1,11 @@
+/**
+https://cassidoo.co/
+**/
+
+/* Dark Mode */
+function setModeEventListener() {
+  let list = document.body.classList;
+  document.getElementById("toggler").addEventListener("change", event => {
+    event.target.checked ? list.add("dark-mode") : list.remove("dark-mode");
+  });
+}
